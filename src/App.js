66fe56cab82789b4b9cdb44e365abbb9ad164ed2
Pranjal Varshney <pranjal25varshney@gmail.com>
2021-02-16 00:55:15 +0530

    app css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routing } from './components/Routing'
+import "./App.css"
 
 export default function App() {
     return (
@@ -8,4 +9,3 @@ export default function App() {
         </div>
     )
 }
-    
