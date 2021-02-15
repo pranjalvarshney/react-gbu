8e754ab7323d9487114f91d01b6c79d7522acb3b
Pranjal Varshney <pranjal25varshney@gmail.com>
2021-02-16 00:34:56 +0530

    routing added

--- src/components/routing.js
+++ src/components/routing.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export const routing = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/library" />
+        <Route exact path="/ccc" />
+        <Route exact path="/admissions" />
+        <Route exact path="/campuslife" />
+        <Route exact path="/examination" />
+        <Route exact path="/about" />
+        <Route exact path="/contact" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
